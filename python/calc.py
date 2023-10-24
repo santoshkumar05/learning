b57bae340d91cd931aac160d6b2e920c7a361dd9
santoshkumar prataprao <santoshkumarprataparao@gmail.com>
2023-10-24 17:45:28 +0530

    added calc, modified hw

---
+++ python/calc.py
@@ -0,0 +1,8 @@
+a= 10
+b = 5
+import pdb;pdb.set_trace
+print (a+b)
+print (a-b)
+print (a*b)
+print (a/b)
+print (a%b)
--- python/helloworld.py
+++ python/helloworld.py
@@ -1,8 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Jul 16 11:16:17 2023
 
-@author: santo
-"""
 
 print ("hello world")
