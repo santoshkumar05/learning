ea93d956750468b1fc807c509a9b9d7a3e75ed5a
santoshkumar prataprao <santoshkumarprataparao@gmail.com>
2024-01-11 07:51:52 +0530

    loops

---
+++ python/loops.py
@@ -0,0 +1,19 @@
+# WHILE LOOP 
+n=1
+while n<=10:
+    print(n)
+    n=n+1
+ 
+# CONTINUE 
+for n in range(1,10):
+    if n%2==0:
+        continue
+    else:
+        print(n)
+# BREAK 
+for i in range(1,10):
+    print(i)
+    if i==5:
+        break
+
+        
